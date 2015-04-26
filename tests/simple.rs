df7d4b626e71d30d9852789ec98cf1e522151800
Ning Sun <sunng@about.me>
2015-04-26 14:26:56 +0800

    (test) added Option in example

--- tests/simple.rs
+++ tests/simple.rs
@@ -8,12 +8,14 @@ use rustc_serialize::json::{Json, ToJson};
 #[derive(ToJson)]
 struct Person {
     name: String,
-    age: u8
+    age: u8,
+    nationality: Option<String>
 }
 
 #[test]
 fn test_gen () {
-    let p = Person { name: "Vidar Kjartansson".to_string(), age: 25 };
+    let p = Person { name: "Vidar Kjartansson".to_string(), age: 25,
+                     nationality: Some("Iceland".to_string())};
     let pj = p.to_json();
 
     println!("{}", pj);

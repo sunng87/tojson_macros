ad7b109885185fa7973a95039e6e4c26546f0ad2
Ning Sun <sunng@about.me>
2016-10-02 14:41:23 +0800

    (feat) reimplement with macros-1.1
    Signed-off-by: Ning Sun <sunng@about.me>

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,5 +1,7 @@
-#![feature(custom_derive, plugin)]
-#![plugin(tojson_macros)]
+#![feature(rustc_macro)]
+
+#[macro_use]
+extern crate tojson_macros;
 
 extern crate rustc_serialize;
 
@@ -9,19 +11,24 @@ use rustc_serialize::json::{Json, ToJson};
 struct Person {
     name: String,
     age: u8,
-    nationality: Option<String>
+    nationality: Option<String>,
 }
 
 #[derive(ToJson)]
-struct Person2<'a, T> where T: ToJson {
+struct Person2<'a, T>
+    where T: ToJson
+{
     name: &'a str,
-    age: T
+    age: T,
 }
 
 #[test]
-fn test_gen () {
-    let p = Person { name: "Vidar Kjartansson".to_string(), age: 25,
-                     nationality: Some("Iceland".to_string())};
+fn test_gen() {
+    let p = Person {
+        name: "Vidar Kjartansson".to_string(),
+        age: 25,
+        nationality: Some("Iceland".to_string()),
+    };
     let pj = p.to_json();
 
     println!("{}", pj);
@@ -32,10 +39,14 @@ fn test_gen () {
                *pj.find("nationality").unwrap());
 
     let p2_name = "Solvi Ottesen";
-    let p2 = Person2::<u8> {name: p2_name, age: 31};
+    let p2 = Person2::<u8> {
+        name: p2_name,
+        age: 31,
+    };
     let pj2 = p2.to_json();
     println!("{}", p2.to_json());
 
-    assert_eq!(Json::String("Solvi Ottesen".to_string()), *pj2.find("name").unwrap());
+    assert_eq!(Json::String("Solvi Ottesen".to_string()),
+               *pj2.find("name").unwrap());
     assert_eq!(Json::U64(31), *pj2.find("age").unwrap());
 }
